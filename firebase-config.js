82ee4b01c0b38f933a88b364b86e71fdde57b64c
nickmuff <nickmuff09@gmail.com>
2026-05-18 17:02:25 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-  apiKey: "COLE_SUA_API_KEY_AQUI",
-  authDomain: "SEU-PROJETO.firebaseapp.com",
-  projectId: "SEU-PROJETO",
-  storageBucket: "SEU-PROJETO.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:xxxxxxxxxxxxxxxx"
+  apiKey: "AIzaSyAk7mKhpNsft2EEgEjYLpWOBGoaGPFs910",
+  authDomain: "crriar.firebaseapp.com",
+  projectId: "crriar",
+  storageBucket: "crriar.firebasestorage.app",
+  messagingSenderId: "805880400970",
+  appId: "1:805880400970:web:9443ac3d070356da0deab3",
+  measurementId: "G-76MPWN94T6"
 };
 
 firebase.initializeApp(firebaseConfig);
