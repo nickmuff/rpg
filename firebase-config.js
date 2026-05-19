5a9ab0425d0a9906fa156b4c32d1f5244c76981d
nickmuff <nickmuff09@gmail.com>
2026-05-18 23:24:42 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,17 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAk7mKhpNsft2EEgEjYLpWOBGoaGPFs910",
-  authDomain: "crriar.firebaseapp.com",
-  projectId: "crriar",
-  storageBucket: "crriar.firebasestorage.app",
-  messagingSenderId: "805880400970",
-  appId: "1:805880400970:web:9443ac3d070356da0deab3",
-  measurementId: "G-76MPWN94T6"
+  apiKey: "AIzaSyDkJK1vpKyMjqsiq2Rj4xRaIdn4CzZmYBI",
+  authDomain: "nectoflora.firebaseapp.com",
+  projectId: "nectoflora",
+  storageBucket: "nectoflora.firebasestorage.app",
+  messagingSenderId: "801628668001",
+  appId: "1:801628668001:web:61b9f993f379cfade0ea20"
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+// Exportar instâncias (opcional, mas útil)
 const auth = firebase.auth();
 const db = firebase.firestore();
+
+console.log("✅ Firebase inicializado!");
